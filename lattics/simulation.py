29fdaf5929c48e2c7a71a99b5bcf52b940877b67
Daniel Kiss <kissdanieldezso@gmail.com>
2024-02-28 10:06:42 +0100

    Simulation added.

---
+++ lattics/simulation.py
@@ -0,0 +1,16 @@
+"""The main component of the LattiCS framework containing functionalities to set up and execute a simulation.
+"""
+
+import agent
+
+class Simulation:
+    def __init__(self):        
+        pass
+
+    def initialize(self):
+        pass
+
+    def run(self):
+        pass
+
+    
