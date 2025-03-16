3670e05904737a4669bb407c1feb2d610ebd0d81
Daniel Kiss <kissdanieldezso@gmail.com>
2025-03-16 11:32:14 +0100

    Changes in flag naming in FixedIncrementalCellCycle

--- lattics/cellfunctions.py
+++ lattics/cellfunctions.py
@@ -1,5 +1,6 @@
 
 from .agent import Agent
+import numpy as np
 import warnings
 
 
@@ -23,7 +24,7 @@ class CellFunctionModel:
             self._update()
             self._time_since_last_update -= self._update_interval
 
-    def reset(self) -> None:
+    def _reset(self) -> None:
         pass
 
     def _update(self) -> None:
@@ -34,20 +35,32 @@ class FixedIncrementalCellCycle(CellFunctionModel):
     def __init__(self,
                  update_interval: int,
                  agent: Agent,
-                 length: int
+                 length: int,
+                 initial_time: int = 0,
+                 random_initial: bool = False
                  ):
         super().__init__(update_interval, agent)
         self._length = length
-        self._current_time = 0
+        self._current_time = initial_time
+        if random_initial:
+            self._current_time = np.random.uniform(low=0, high=length)
+
+    @property
+    def current_time(self) -> int:
+        return self._current_time
 
     def initialize_agent_state_flags(self) -> None:
-        self._agent.initialize_status_flag('cell_cycle_complete')
-        self._agent.set_status_flag('cell_cycle_complete', False)
+        self._agent.initialize_status_flag('division_pending', False)
+        self._agent.initialize_status_flag('division_completed', False)
 
-    def reset(self):
+    def _reset(self) -> None:
         self._current_time = 0
+        self._agent.set_status_flag('division_pending', False)
+        self._agent.set_status_flag('division_completed', False)
 
-    def _update(self):
+    def _update(self) -> None:
+        if self._agent.get_status_flag('division_completed'):
+            self._reset()
         self._current_time += self._time_since_last_update
         if self._length <= self._current_time:
-            self._agent.set_status_flag('cell_cycle_complete', True)
+            self._agent.set_status_flag('division_pending', True)
