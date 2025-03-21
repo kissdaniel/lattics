b806b8ffc2de97d9518154536d2da01884579d25
Daniel Kiss <kissdanieldezso@gmail.com>
2025-03-21 15:01:43 +0100

    Added documentation for SimulationDomain and UnstructuredSimulationDomain

--- lattics/domains.py
+++ lattics/domains.py
@@ -6,43 +6,116 @@ import warnings
 
 
 class SimulationDomain:
+    """Acts as a base class for different simulation domains.
+    """
     def __init__(self,
                  simulation: Simulation
                  ) -> None:
+        """Constructor method.
+
+        Parameters
+        ----------
+        simulation : Simulation
+            The Simulation instance containing the domain
+        """
         self._simulation = simulation
 
     def initialize(self) -> None:
+        """Initializes the domain.
+        """
         pass
 
     def add_agent(self, agent: Agent) -> None:
+        """Adds the specified agent to the structure representing the internal
+        storage of the domain.
+
+        Parameters
+        ----------
+        agent : Agent
+            The agent to be added
+        """
         pass
 
     def remove_agent(self, agent: Agent) -> None:
+        """Removes the specified agent from the internal storage of the domain.
+
+        Parameters
+        ----------
+        agent : Agent
+            The agent to be removed
+        """
         pass
 
     def update(self, dt: int) -> None:
+        """Updates the domain according to the specified time duration.
+
+        Parameters
+        ----------
+        dt : int
+            The time elapsed since the last update call, in milliseconds
+        """
         pass
 
 
 class UnstructuredSimulationDomain(SimulationDomain):
+    """Represents a perfectly mixed simulation domain with no spatial structure
+    or localized interactions, where each agent has an equal probability of
+    interacting with any other agent in the population. Agents are stored in a
+    list.
+    """
     def __init__(self,
                  simulation: Simulation,
                  capacity: int = None
                  ) -> None:
+        """Constructor method.
+
+        Parameters
+        ----------
+        simulation : Simulation
+            The Simulation instance containing the domain
+        capacity : int, optional
+            The maximum number of agents in the population. Defaults to None,
+            meaning no limit (infinite population).
+        """
         super().__init__(simulation)
         self._capacity = capacity
         self._agents = list()
 
     def initialize(self) -> None:
+        """Initializes the domain.
+        """
         pass
 
     def add_agent(self, agent: Agent) -> None:
+        """Adds the specified agent to the internal list of the agents.
+
+        Parameters
+        ----------
+        agent : Agent
+            The agent to be added
+        """
         self._agents.append(agent)
 
     def remove_agent(self, agent: Agent) -> None:
+        """Removes the specified agent from the internal list of the agents.
+
+        Parameters
+        ----------
+        agent : Agent
+            The agent to be removed
+        """
         self._agents.remove(agent)
 
     def update(self, dt: int) -> None:
+        """Updates the domain according to the specified time duration. Agents
+        labeled as ``division_pending`` are duplicated based on the available
+        space in the domain, constrained by ``capacity``.
+
+        Parameters
+        ----------
+        dt : int
+            The time elapsed since the last update call, in milliseconds
+        """
         for a in self._agents:
             if a.get_status_flag('division_pending'):
                 self._perform_cell_division(a)
