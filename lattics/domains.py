368038cf566c528b2cf6e551c0d61b005ca4adf4
Daniel Kiss <kissdanieldezso@gmail.com>
2025-03-21 09:46:07 +0100

    UnstructuredSimulationDomain added

--- lattics/domains.py
+++ lattics/domains.py
@@ -25,7 +25,40 @@ class SimulationDomain:
 
 
 class UnstructuredSimulationDomain(SimulationDomain):
-    pass
+    def __init__(self,
+                 simulation: Simulation,
+                 capacity: int = None
+                 ) -> None:
+        super().__init__(simulation)
+        self._capacity = capacity
+        self._agents = list()
+
+    def initialize(self) -> None:
+        pass
+
+    def add_agent(self, agent: Agent) -> None:
+        self._agents.append(agent)
+
+    def remove_agent(self, agent: Agent) -> None:
+        self._agents.remove(agent)
+
+    def update(self, dt: int) -> None:
+        for a in self._agents:
+            if a.get_status_flag('division_pending'):
+                self._perform_cell_division(a)
+
+    def _perform_cell_division(self, agent: Agent) -> None:
+        if np.random.random() < self._get_division_probability():
+            agent.set_status_flag('division_pending', False)
+            agent.set_status_flag('division_completed', True)
+            new_agent = agent.clone()
+            self._simulation.add_agent(new_agent)
+
+    def _get_division_probability(self) -> float:
+        if self._capacity:
+            return 1.0 - len(self._agents) / self._capacity
+        else:
+            return 1.0
 
 
 class Structured2DSimulationDomain(SimulationDomain):
