6085800b3870195782b58096a5246bb62822583d
Daniel Kiss <kissdanieldezso@gmail.com>
2025-08-08 14:34:32 +0200

    SubstrateField added

--- lattics/substrate_field.py
+++ lattics/substrate_field.py
@@ -1,8 +1,6 @@
 import numpy as np
-import copy
-import numba
-from numba import void, float32, int32, boolean, types
 from . import numba_functions
+
 
 class SubstrateField:
     """Base class for different substrate fields.
@@ -22,6 +20,9 @@ class SubstrateField:
     def add_node(self, node) -> None:
         self._substrate_nodes.append(node)
 
+    def get_concentration(self, position) -> float:
+        pass
+
     def update(self, dt: int) -> None:
         pass
 
@@ -35,25 +36,41 @@ class SubstrateFieldNoDimension(SubstrateField):
     def initialize(self) -> None:
         self._concentration_layer = 0
 
+    def get_concentration(self, position) -> float:
+        return self._concentration_layer
+
     def update(self, dt: int) -> None:
         self.update_nodes(dt)
+        self.diffusion_decay(dt)
 
     def update_nodes(self, dt: int) -> None:
+        count_fixed = 0
+        sum_fixed = 0
+        v_f = self._volume
         for n in self._substrate_nodes:
             typ = n['type']
             if typ == 'flux':
-                r_u = n['uptake_rate']
-                r_r = n['release_rate']
+                k_p = n['passive_rate']
+                k_u = n['uptake_rate']
+                k_r = n['release_rate']
                 c_n_cur = n['concentration']
-                c_n_max = n['maximal_concentration']
+                v_n = n['volume']
                 c_f_cur = self._concentration_layer
-                dc = (r_u * c_f_cur * ((c_n_max - c_n_cur) / c_n_max) - r_r * c_n_cur) * dt
-                c_n_new = c_n_cur + dc
-                c_f_new = c_f_cur - dc
+                dn = (k_p * (c_f_cur - c_n_cur) + k_u * c_f_cur - k_r * c_n_cur) * dt
+                c_n_new = c_n_cur + dn / v_n
+                c_f_new = c_f_cur - dn / v_f
                 n['concentration'] = c_n_new
                 self._concentration_layer = c_f_new
             elif typ == 'fixed':
-                self._concentration_layer = n['concentration'] / self._volume
+                sum_fixed = sum_fixed + n['concentration']
+                count_fixed = count_fixed + 1
+        if 0 < count_fixed:
+            self._concentration_layer = sum_fixed / count_fixed
+
+    def diffusion_decay(self, dt: int) -> None:
+        C = self._concentration_layer
+        d = self._substrate_data['decay_coefficient']
+        self._concentration_layer = C * np.exp(-d * dt)
 
 
 class SubstrateField2D(SubstrateField):
@@ -65,25 +82,30 @@ class SubstrateField2D(SubstrateField):
         super().__init__(domain)
 
     def initialize(self):
-        self._concentration_layer = np.zeros((self._dim_x + 2, self._dim_y + 2), dtype=np.float32)
+        self._concentration_layer = np.zeros((self._dim_x, self._dim_y), dtype=np.float32)
+
+    def get_concentration(self, position) -> float:
+        return self._concentration_layer[position]
 
     def update(self, dt: int) -> None:
         self.update_nodes(dt)
         self.diffusion_decay(dt)
 
     def update_nodes(self, dt: int) -> None:
+        v_f = self._dx**2
         for n in self._substrate_nodes:
             pos = n['position']
             typ = n['type']
             if typ == 'flux':
-                r_u = n['uptake_rate']
-                r_r = n['release_rate']
+                k_p = n['passive_rate']
+                k_u = n['uptake_rate']
+                k_r = n['release_rate']
                 c_n_cur = n['concentration']
-                c_n_max = n['maximal_concentration']
+                v_n = n['volume']
                 c_f_cur = self._concentration_layer[pos]
-                dc = (r_u * c_f_cur * ((c_n_max - c_n_cur) / c_n_max) - r_r * c_n_cur) * dt
-                c_n_new = c_n_cur + dc
-                c_f_new = c_f_cur - dc
+                dn = (k_p * (c_f_cur - c_n_cur) + k_u * c_f_cur - k_r * c_n_cur) * dt
+                c_n_new = c_n_cur + dn / v_n
+                c_f_new = c_f_cur - dn / v_f
                 n['concentration'] = c_n_new
                 self._concentration_layer[pos] = c_f_new
             elif typ == 'fixed':
@@ -91,10 +113,7 @@ class SubstrateField2D(SubstrateField):
 
     def diffusion_decay(self, dt: int) -> None:
         C = self._concentration_layer
-        shape_x = self._concentration_layer.shape[0]
-        shape_y = self._concentration_layer.shape[1]
         D = self._substrate_data['diffusion_coefficient']
         d = self._substrate_data['decay_coefficient']
         dx = self._dx
-        # numba_functions.diffusion_solver_ftcs_2d(C, shape_x, shape_y, D, d, dt, dx)
-        numba_functions.diffusion_solver_pr_adi_2d(C, shape_x, shape_y, D, d, dt, dx)
+        self._concentration_layer = numba_functions.diffusion_solver_lod_2d(C, D, d, np.float32(dt), dx)
