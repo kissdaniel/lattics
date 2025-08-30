2dc92d58aa9564b9a91637d21b7cac671ea7cb80
Daniel Kiss <kissdanieldezso@gmail.com>
2025-08-30 17:52:35 +0200

    UnitConverter added to utils

---
+++ lattics/utils.py
@@ -0,0 +1,18 @@
+class UnitConverter:
+    TIME_UNITS = {
+        'ms': 1,
+        'sec': 1000,
+        'min': 60 * 1000,
+        'hour': 60 * 60 * 1000,
+        'day': 24 * 60 * 60 * 1000,
+        'week': 7 * 24 * 60 * 60 * 1000
+    }
+
+    @staticmethod
+    def time(expression: tuple[float, str]) -> int:
+        value = expression[0]
+        unit = expression[1]
+        if unit in UnitConverter.TIME_UNITS:
+            return value * UnitConverter.TIME_UNITS[unit]
+        else:
+            raise ValueError(f"Invalid time unit: '{unit}'. Supported units are: 'ms', 'sec', 'min', 'hour', 'day', 'week'.")
