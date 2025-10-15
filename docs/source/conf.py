1b3e040f465d60881faa93bf4a02101f1d8191e7
Daniel Kiss <kissdanieldezso@gmail.com>
2025-10-15 12:53:23 +0200

    Added Sphinx subfigures

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,6 +43,7 @@ extensions = [
     "matplotlib.sphinxext.plot_directive",
     "numpydoc",
     "sphinx_copybutton",
+    "sphinx_subfigure",
 ]
 
 # Configuration options for plot_directive. See:
